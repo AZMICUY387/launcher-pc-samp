df98df32d88dc2d601b66d21b8b9c5ccf053458e
Azmi387 <55588164+AZMICUY387@users.noreply.github.com>
2021-05-23 03:08:59 +0700

    samp launcher

---
+++ pc samp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace pc_samp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(901, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 136);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 600);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(901, 454);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "NickName";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(1059, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 109);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pc_samp.Properties.Resources.wallpaperflare_com_wallpaper__3_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1182, 648);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "LAUNCHER PC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pc samp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pc_samp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ServerIP = "127.0.0.1:7777 ";//put your ip and port in this line
+            string SPHash = ""; //PASSWORD
+            string GTAPath = Registry.CurrentUser.OpenSubKey(@"Software\\SAMP").GetValue("gta_sa_exe").ToString();
+            GTAPath = GTAPath.Substring(0, GTAPath.LastIndexOf(@"\") + 1);
+            string[] ImaCheat1 = Directory.GetDirectories(GTAPath, "cleo");
+            string[] ImaCheat2 = Directory.GetFiles(GTAPath, "CLEO.asi");
+            string[] ImaCheat3 = Directory.GetFiles(GTAPath, "d3d9.dll");
+            string[] ImaCheat4 = Directory.GetFiles(GTAPath, "SAMPFUNCS.asi");
+            string[] ImaCheat5 = Directory.GetDirectories(GTAPath, "mod_sa");
+            string[] ImaCheat6 = Directory.GetDirectories(GTAPath, "OverLight_Mod");
+            string[] ImaCheat7 = Directory.GetFiles(GTAPath, "vorbishooked.dll");
+            if (((ImaCheat1.Length > 0) || (ImaCheat2.Length > 0)) || ((ImaCheat3.Length > 0) || (ImaCheat4.Length > 0)))
+            {
+                MessageBox.Show("HAPUS DULU CHEATNYA!!!!!", "CHEAT DITEMUKAN!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if ((ImaCheat5.Length > 0) || (ImaCheat6.Length > 0))
+            {
+                MessageBox.Show("HAPUS DULU CHEATNYA!!!!!", "CHEAT DITEMUKAN!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else if (ImaCheat7.Length > 0)
+            {
+                MessageBox.Show("HAPUS DULU CHEATNYA!!!!!", "CHEAT DITEMUKAN!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else
+            {
+                Registry.CurrentUser.OpenSubKey(@"Software\SAMP", true).SetValue("PlayerName", textBox1.Text);
+                Process.Start(GTAPath + "samp.exe", ServerIP + SPHash);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
